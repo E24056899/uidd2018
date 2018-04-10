51b03ced06fc45b53d4ee6e4ca093c07f68a22a4
E24056899 <samantha88.tw@gmail.com>
2018-04-11 03:33:59 +0800

    0411update

--- hw3.js
+++ hw3.js
@@ -1,17 +1,18 @@
-$("#lottery_box").click(function()){
+$("#left").click(function(){
   var img_num=Math.floor(Math.random()*6);
   var imgurl=[
     "https://luffy.ee.ncku.edu.tw/~E24056899/hw3pic/1_red.png",
-    "https://luffy.ee.nclu.edu.tw/~E24056899/hw3pic/2_orange.png",
+    "https://luffy.ee.ncku.edu.tw/~E24056899/hw3pic/2_orange.png",
     "https://luffy.ee.ncku.edu.tw/~E24056899/hw3pic/3_yellow.png",
     "https://luffy.ee.ncku.edu.tw/~E24056899/hw3pic/4_green.png",
     "https://luffy.ee.ncku.edu.tw/~E24056899/hw3pic/5_blue.png",
-    "https://luffy.ee.ncku.edu.tw/~E24056899/hw3pic/6_gray.png",];
-  $("#stick").animate({top:"-200px",left:"450px",opacity:"5.0"},1000,$.fn.showresult);
-  $.fn.showresult=function(){
-    $("#result").html('<img src="'+imgurl[img_num]+'">');
-    $("#result").css("left","100px");
-    $("#result").css("top","-280px");
-  };
+    "https://luffy.ee.ncku.edu.tw/~E24056899/hw3pic/6_gray.png"
+  ];
+  $("#stick").animate({top:"-500px",left:"160px"},1000,function(){
+    $("#paper").html('<img src="'+imgurl[img_num]+'">');
+    $("#paper").css("left","480px");
+    $("#paper").css("top","200px");
+  }
+  );
 }
-};
+);
